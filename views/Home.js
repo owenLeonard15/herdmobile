6125f933c5f2ec040da70079b9e93b9ca5ce1ad1
Xingbei99 <xingbei.cao@vanderbilt.edu>
2021-03-27 04:31:53 -0500

    Backend for fetching a list of nearby restaurants

--- views/Home.js
+++ views/Home.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native'
 
 const HomeScreen = () => {
+    // Store and manage the fetched restaurant list.
+    const [restaurantList, setRestaurantList] = useState([]);
+
+    // Initialize the restaurant list.
+    useEffect(() => {
+        const init = async() => {
+            const resList = await getRestaurantsFromApi();
+            setRestaurantList(restaurantList => {
+                for (var i = 0; i < resList.length; i++){
+                    restaurantList.push(resList[i]);
+                }
+            })
+            console.log(restaurantList);
+        };
+
+        init();
+    }, []);
+
+    // Fetch restaurants with Google Places API under the hood.
+    const getRestaurantsFromApi = async() => {
+      const res = await fetch('https://i7vva9aayi.execute-api.us-east-2.amazonaws.com/dev/restaurants');
+      const resJson = await res.json();
+      return resJson.items;
+    }
+
     return (
         <View style={styles.container}>
             <Text>Home</Text>
